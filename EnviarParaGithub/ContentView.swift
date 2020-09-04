471d9d9b3420ccc8201414438d7d93c5c1b27a99
Usuário Convidado <Guest@L2602MICRO09.local>
2020-09-03 23:13:18 -0300

    Initial Commit

---
+++ EnviarParaGithub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnviarParaGithub
+//
+//  Created by Usuário Convidado on 03/09/20.
+//  Copyright © 2020 WillCarvalho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
